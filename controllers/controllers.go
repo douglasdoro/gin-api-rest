3ec4c472157ef6eb5d2523b395c6ad3452063460
Douglas Fontes <douglasdoro@gmail.com>
2022-04-24 13:47:21 -0300

    add endpoint to add new alunos

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"gi-api-rest/database"
 	"gi-api-rest/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,4 +17,18 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz": "E ai " + nome + ", tudo bem?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusCreated, aluno)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,5 +10,6 @@ func HandleRequest() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET(":nome", controllers.Saudacao)
+	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.Run()
 }
